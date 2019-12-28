5e5218d5ad3eb2b5a9b66cea0b1e0d570749bdf4
Karen V <karenvo@usc.edu>
2019-12-28 04:34:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ function moveDodgerLeft() {
 //     }
 //   });
 
-function moveDodgerRight() {
-  let leftNumbers = dodger.style.left.replace("", "px");
-  let left = parseInt(leftNumbers, 10);
-
-  if (left > 0) {
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowRight") {
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+ 
     dodger.style.left = `${left + 1}px`;
   }
-}
+});
